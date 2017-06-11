732f0df67ff05d084a64aa41cb83baf73a72ff28
Jhonn <jhonn1995@gmail.com>
2017-06-11 18:50:46 -0300

    Update

--- config.sh
+++ config.sh
@@ -13,6 +13,24 @@ if [[ DISTRO == "centos" ]]; then
   yum -y update #Install Updates
   #Install dependencies
   yum -y install zsh git vim wget mlocate epel-release htop
+  updatedb #update db for mlocate
+  #Default shell
+  echo "export SHELL=/bin/zsh
+  exec /bin/zsh -l" >> /home/$USER/.bash_profile
+  sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+  cd /home/$USER/
+  wget https://raw.githubusercontent.com/JohnWillker/dotfiles/master/.vimrc
+
+fi
+
+if [[ DISTRO == "debian" ]]; then
+  echo "Install configs for debian based"
+  sleep 2
+  echo "..."
+
+  apt-get -y update #Install Updates
+  #Install dependencies
+  apt-get -y install zsh git vim wget htop
   #Default shell
   echo "export SHELL=/bin/zsh
   exec /bin/zsh -l" >> /home/$USER/.bash_profile
