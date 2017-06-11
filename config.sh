b60b7c6a9fb36947705094492c1ab99a774de256
Jhonn <jhonn1995@gmail.com>
2017-06-11 17:41:19 -0300

    Add config.sh

---
+++ config.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#linux basic tools
+#Author: Jhonn Willker F. F. (frazao.jhonn@gmail.com)
+#install vim and .vimrc git etc..
+#Work with centos or debian based.
+python -mplatform | grep -qi  centos && echo "yes" || echo"no"
