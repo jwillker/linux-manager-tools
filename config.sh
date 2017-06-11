1b84f0837b6cdd59ab159a19f44206fc6758dc78
Jhonn <jhonn1995@gmail.com>
2017-06-11 18:47:05 -0300

    Centos Function

--- config.sh
+++ config.sh
@@ -3,4 +3,21 @@
 #Author: Jhonn Willker F. F. (frazao.jhonn@gmail.com)
 #install vim and .vimrc git etc..
 #Work with centos or debian based.
-python -mplatform | grep -qi  centos && echo "yes" || echo"no"
+python -mplatform | grep -qi centos && DISTRO="centos" || DISTRO="debian"
+
+if [[ DISTRO == "centos" ]]; then
+  echo "Install configs for centos based"
+  sleep 2
+  echo "..."
+
+  yum -y update #Install Updates
+  #Install dependencies
+  yum -y install zsh git vim wget mlocate epel-release htop
+  #Default shell
+  echo "export SHELL=/bin/zsh
+  exec /bin/zsh -l" >> /home/$USER/.bash_profile
+  sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+  cd /home/$USER/
+  wget https://raw.githubusercontent.com/JohnWillker/dotfiles/master/.vimrc
+
+fi
